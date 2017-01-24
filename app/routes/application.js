60d95d4e4d8ddcbee4f674ef56bb7e3895531a71
maxiehernandez <maxinehernandez@hotmail.com>
2017-01-24 12:41:56 -0500

    Added serializer and created models to get db data from rails.

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,19 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return [{
-      name: 'Sparkachu',
-      level: 3
-    }, {
-      name: 'Charember',
-      level: 29
-    }, {
-      name: 'Rhauk',
-      level: 5
-    }, {
-      name: 'Poddle',
-      level: 9
-    }]
+    return this.store.findAll('monster');
+
+    // [{
+    //   name: 'Sparkachu',
+    //   level: 3
+    // }, {
+    //   name: 'Charember',
+    //   level: 29
+    // }, {
+    //   name: 'Rhauk',
+    //   level: 5
+    // }, {
+    //   name: 'Poddle',
+    //   level: 9
+    // }]
     // [
     //   2, 3, 6, 9
     // ]
