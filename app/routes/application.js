a40a736bb6e113e951fa3773617d0d3e90da6b59
maxiehernandez <maxinehernandez@hotmail.com>
2017-01-24 11:51:20 -0500

    Ember basics to connect to rails.

---
+++ app/routes/application.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(){
+    return [{
+      name: 'Sparkachu',
+      level: 3
+    }, {
+      name: 'Charember',
+      level: 29
+    }, {
+      name: 'Rhauk',
+      level: 5
+    }, {
+      name: 'Poddle',
+      level: 9
+    }]
+    // [
+    //   2, 3, 6, 9
+    // ]
+    // {
+    //   sentence: 'This is our Model',
+    //   sentence2: 'This is our sentence2'
+    // }
+  }
+});
